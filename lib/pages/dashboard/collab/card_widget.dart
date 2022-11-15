70fdc07cea020d5a4b2728e9377f9011c4c79e64
matiasga98 <matiasgamal98@gmail.com>
2022-11-15 02:30:23 -0300

    fix: negrita y texto card collabs

--- lib/pages/dashboard/collab/card_widget.dart
+++ lib/pages/dashboard/collab/card_widget.dart
@@ -7,17 +7,17 @@ import 'package:get/get.dart';
 import '../../../config/colors_.dart';
 
 class CollabCard extends StatelessWidget {
-  CollabCard({
-    Key? key,
-    required this.open,
-    required this.finalImage,
-    required this.name,
-    required this.description,
-    required this.address,
-    required this.instruments,
-    required this.genres,
-    required this.collabInstrument
-  }) : super(key: key);
+  CollabCard(
+      {Key? key,
+      required this.open,
+      required this.finalImage,
+      required this.name,
+      required this.description,
+      required this.address,
+      required this.instruments,
+      required this.genres,
+      required this.collabInstrument})
+      : super(key: key);
 
   final String finalImage;
   final String name;
@@ -29,7 +29,6 @@ class CollabCard extends StatelessWidget {
   String collabImage = "";
   String collabName = "";
   bool open;
-
 
   @override
   Widget build(BuildContext context) {
@@ -45,126 +44,125 @@ class CollabCard extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
                 Center(
-                  child : Text(
+                  child: Text(
                     name.split(" ").first,
                     style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
+                        fontSize: 12,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
                     maxLines: 1,
                   ),
                 ),
                 Center(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Image.memory(
-                      base64Decode(finalImage),
-                      width: 75,
-                      height: 75,
-                      fit: BoxFit.cover,
-                    ))
-                )
-                ]
-            )
-          ),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Image.memory(
+                          base64Decode(finalImage),
+                          width: 75,
+                          height: 75,
+                          fit: BoxFit.cover,
+                        )))
+              ])),
           const SizedBox(
             width: 16,
           ),
           Expanded(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                Center(
+                    child: Text(
+                  "Estado",
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                )),
+                Center(
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Image.asset(
+                          open ? "images/Abierto.jpg" : "images/Cerrado.jpg",
+                          width: 75,
+                          height: 75,
+                          fit: BoxFit.fitWidth,
+                        )))
+              ])),
+          Expanded(
+              child: Center(
+                  child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "Esta buscando:",
+                style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 75,
+                width: 75,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Center(
-                     child: Text(
-                      "Estado",
-                      style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                      maxLines: 1,
-                    )
-                    ),
-                    Center(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          open? "images/Abierto.jpg" : "images/Cerrado.jpg",
-                          width: 75,
-                          height: 75,
-                          fit: BoxFit.fitWidth,
-                        )
-                      )
-                    )
-                  ]
-              )
-              )
-          ,
-          Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "Esta buscando:",
-                    style: TextStyle(fontSize: 12, color: Colors.black),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        getInstrumentNames(),
-                        style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold),
-                        maxLines: 1,
+                    Flexible(
+                      flex: 1,
+                      child: FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Text(
+                          getInstrumentNames(),
+                          style: TextStyle(fontSize: 12, color: Colors.black),
+                          maxLines: 1,
+                        ),
                       ),
                     ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        getGenreNames(),
-                        style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold),
-                        maxLines: 2,
+                    Flexible(
+                      flex: 1,
+                      child: FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Text(
+                          getGenreNames(),
+                          style: TextStyle(fontSize: 12, color: Colors.black),
+                          maxLines: 2,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ))
+                  ],
+                ),
+              )
+            ],
+          ))),
         ],
       ),
     );
   }
 
-  String getInstrumentNames(){
+  String getInstrumentNames() {
     String names = " ";
     instruments.forEach((element) {
-      names = names +element +", ";
+      names = names + element + ", ";
     });
-    names = names.length>1? names.substring(0,names.length-2):names;
+    names = names.length > 1 ? names.substring(0, names.length - 2) : names;
 
     return names;
   }
 
-  String getGenreNames(){
+  String getGenreNames() {
     String names = " ";
     genres.forEach((element) {
-      names = names +element +", ";
+      names = names + element + ", ";
     });
-    names = names.length>1? names.substring(0,names.length-2):names;
+    names = names.length > 1 ? names.substring(0, names.length - 2) : names;
 
     return names;
   }
